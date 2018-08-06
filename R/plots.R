b02594c376bfff3e93285a0c575508e54bc22837
renatoinomata <renato.inomata@gmail.com>
2018-08-06 17:35:14 -0300

    Acentos

--- R/plots.R
+++ R/plots.R
@@ -2,9 +2,9 @@ plot.hidrograma <- function(dados){
   Plot <- ggplot2::ggplot(dados, ggplot2::aes(x = Data, y = Q)) +
     ggplot2::geom_line(ggplot2:: aes(color = Estacao)) +
     ggplot2::theme_bw() +
-    ggplot2::labs(title = "Hidrograma", x = "Data", y = "Q (mÂ³/s)")
+    ggplot2::labs(title = "Hidrograma", x = "Data", y = "Q (m³/s)")
   return(Plot)
-  # falta alterar a Estacao para EstaÃ§Ã£o
+  # falta alterar a Estacao para Estação
 }
 
 plot.cperm <- function(cperm){
@@ -16,8 +16,8 @@ plot.cperm <- function(cperm){
                                                               function(x) 10^x),
                        labels = scales::trans_format("log10",
                                                      scales::math_format(10^.x))) +
-    ggplot2::labs(x = "FrequÃªncia (%)", y = "Q (mÂ³/s)",
-         title = paste0("Curva de permanÃªncia")) +
+    ggplot2::labs(x = "Frequência (%)", y = "Q (m³/s)",
+         title = paste0("Curva de permanência")) +
     ggplot2::annotation_logticks(sides = "l", scaled = FALSE)
 }
 
@@ -26,13 +26,13 @@ plot.boxplot <- function(dados){
     Plot <- ggplot2::ggplot(dados, ggplot2::aes(x = Estacao, y = Q)) +
       ggplot2::geom_boxplot() +
       ggplot2::theme_bw() +
-      ggplot2::labs(title = "Boxplot", x = "EstaÃ§Ã£o", y = "Q (mÂ³/s)")
+      ggplot2::labs(title = "Boxplot", x = "Estação", y = "Q (m³/s)")
   }
   if(any(colnames(dados) == "P")){
     Plot <- ggplot2::ggplot(dados, ggplot2::aes(x = Estacao, y = P)) +
       ggplot2::geom_boxplot() +
       ggplot2::theme_bw() +
-      ggplot2::labs(title = "Boxplot", x = "EstaÃ§Ã£o", y = "P (mm)")
+      ggplot2::labs(title = "Boxplot", x = "Estação", y = "P (mm)")
   }
   return(Plot)
 }
@@ -45,7 +45,7 @@ plot.histograma <- function(dados){
                               position = "identity",
                               ggplot2::aes(fill = Estacao), bins = 30) +
       ggplot2::theme_bw() +
-      ggplot2::labs(title = "Histograma", x = "Q (mÂ³/s)", y = "n")
+      ggplot2::labs(title = "Histograma", x = "Q (m³/s)", y = "n")
   }
   if(any(colnames(dados) == "P")){
     Plot <- ggplot2::ggplot(dados, ggplot2::aes(x = P)) +
