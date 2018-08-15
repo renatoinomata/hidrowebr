533b3eb4f304ad1b448b37c94da152c914d70582
renatoinomata <renato.inomata@gmail.com>
2018-08-15 15:19:45 -0300

    stop ao invés de warning acentos

--- R/importar.R
+++ R/importar.R
@@ -1,20 +1,20 @@
 importar <- function(txt){
-  # Verificação do tipo de arquivo
+  # VerificaÃ§Ã£o do tipo de arquivo
   if(is.character(txt) == FALSE){
-    warning("Variável não é do tipo chr", call. = FALSE)
+    stop("VariÃ¡vel nÃ£o Ã© do tipo chr", call. = FALSE)
   } else {
-    # Verificação da existência dos arquivos
+    # VerificaÃ§Ã£o da existÃªncia dos arquivos
     remove_txt <- c()
     for(i in 1:length(txt)){
       if(file.exists(txt[i]) == FALSE){
-        warning(paste0("Arquivo '", txt[i], "' não existe"))
+        warning(paste0("Arquivo '", txt[i], "' nÃ£o existe"))
         remove_txt <- c(remove_txt, i)
       }
     }
     if(is.null(remove_txt) == FALSE){
       txt <- txt[-remove_txt]
     }
-      # Importação dos dados
+      # ImportaÃ§Ã£o dos dados
       dados <- data.frame()
       for(i in 1:length(txt)){
         dados_aux <- read.table(txt[i],
