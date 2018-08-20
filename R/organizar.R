91411dc10068c657bc777fd1703dd52a629db2ab
renatoinomata <renato.inomata@gmail.com>
2018-08-20 14:16:32 -0300

    Adicionado documentação e leitura troca de coluna para P caso os dados sejam de chuva

--- R/organizar.R
+++ R/organizar.R
@@ -1,15 +1,34 @@
+#' Função que organiza os dados brutos em uma data frame com datas,
+#' vazões/precipitações e estação.
+#'
+#' @param dados Os dados brutos obtidos após a função de \code{\link{importar}}.
+#'
+#' @details Os dados brutos são organizados em uma data frame com: colunas de
+#'   \code{Estacao}, que representa o código da estação; \code{Data}, que é a
+#'   data da leitura; e \code{Q} ou \code{P}, valores de vazão ou precipitação,
+#'   respectivamente.
 organizar <- function(dados){
   # Limpando os dados
+  if(grepl("Vazao", names(dados)[17]))
+    valor <- "Q"
+  else
+    valor <- "P"
   dados <- dados[c(1, 3, 17:47)]
   names(dados) <- c("Estacao", "Data", 1:31)
   dados <- tidyr::separate(dados, Data, into = c("Ano", "Mes", "Dia"))
   dados["Dia"] <- NULL
 
   # Organizando
-  dados <- tidyr::gather(dados, "Dia", "Q", 4:34)
+  if(valor == "Q")
+    dados <- tidyr::gather(dados, "Dia", "Q", 4:34)
+  else
+    dados <- tidyr::gather(dados, "Dia", "P", 4:34)
   dados <- dplyr::arrange(dados, Ano, Mes)
   dados <- tidyr::unite(dados, Dia, Mes, Ano, col = "Data", sep = "/")
-  dados <- dplyr::filter(dados, !is.na(Q))
+  if(valor == "Q")
+    dados <- dplyr::filter(dados, !is.na(Q))
+  else
+    dados <- dplyr::filter(dados, !is.na(P))
   dados$Data <- as.Date(dados$Data, "%d/%m/%Y")
   if(length(unique(dados$Estacao)) > 1){
     dados_bkp <- dados
