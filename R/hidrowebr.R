6b80000940a4e5f638a8b843c7634bad69d615a8
renatoinomata <renato.inomata@gmail.com>
2018-08-21 18:44:26 -0300

    documentação do pacote

---
+++ R/hidrowebr.R
@@ -0,0 +1,22 @@
+#' hidrowebr: Uma ferramenta para análise estátistica de séries hidrológicas.
+#'
+#' As funções do hidrowebr são divididas em três categorias: pré-processamento,
+#' processamento e análise.
+#'
+#' @section Pré-processamento:
+#' As funções de pré-processamento organizam os dados brutos
+#'
+#' \code{\link{importar}}
+#' \code{\link{organizar}}
+#' \code{\link{descartar}}
+#'
+#' @section Processamento:
+#' As funções de processamento
+#'
+#' @section Análise:
+#' As funções de análise
+#' \code{\link{distprob}}
+#'
+#' @docType package
+#' @name hidrowebr
+NULL
