5e6cd07c5c9d2173f2aade88fe50efc556f7bd4c
renatoinomata <renato.inomata@gmail.com>
2018-08-21 18:44:43 -0300

    adicionado read.csv + documentação

--- R/importar.R
+++ R/importar.R
@@ -1,27 +1,41 @@
-importar <- function(txt){
+#' Função que importar os dados de arquivos \code{.arq}.
+#'
+#' @param arq Variável do tipo \code{chr}. Corresponde ao nome ou caminho do
+#'   arquivo a ser importado.
+#'
+#' @details Por meio dessa função serão importados os dados tais quais estão
+#'   dispostos no arquivo original do portal Hidroweb.
+importar <- function(arq){
   # Verificação do tipo de arquivo
-  if(is.character(txt) == FALSE){
+  if(is.character(arq) == FALSE){
     stop("Variável não é do tipo chr", call. = FALSE)
   } else {
     # Verificação da existência dos arquivos
-    remove_txt <- c()
-    for(i in 1:length(txt)){
-      if(file.exists(txt[i]) == FALSE){
-        warning(paste0("Arquivo '", txt[i], "' não existe"))
-        remove_txt <- c(remove_txt, i)
+    remove_arq <- c()
+    for(i in 1:length(arq)){
+      if(file.exists(arq[i]) == FALSE){
+        warning(paste0("Arquivo '", arq[i], "' não existe"))
+        remove_arq <- c(remove_arq, i)
       }
     }
-    if(is.null(remove_txt) == FALSE){
-      txt <- txt[-remove_txt]
+    if(is.null(remove_arq) == FALSE){
+      arq <- arq[-remove_arq]
     }
       # Importação dos dados
       dados <- data.frame()
-      for(i in 1:length(txt)){
-        dados_aux <- read.table(txt[i],
+      for(i in 1:length(arq)){
+        if(grepl("csv", arq[i]))
+          dados_aux <- read.csv(arq[i],
                                 skip = 15,
                                 header = TRUE,
                                 sep = ";",
                                 dec = ",")
+        else
+          dados_aux <- read.table(arq[i],
+                                  skip = 15,
+                                  header = TRUE,
+                                  sep = ";",
+                                  dec = ",")
         dados <- rbind(dados, dados_aux)
       }
       return(dados)
