9583cbf70aa0d425fd85e1484c459fa6174637f9
renatoinomata <renato.inomata@gmail.com>
2018-08-22 13:58:15 -0300

    alterado formato_data

--- R/organizar.R
+++ R/organizar.R
@@ -11,12 +11,12 @@
 #'   data da leitura; e \code{Q} ou \code{P}, valores de vazão (m³/s) ou
 #'   precipitação (mm), respectivamente.
 #'
-#'   O valor padrão para a variável \code{formato_data} é %d/%m/%Y, ou seja,
-#'   dd/mm/AAAA. Para mais informações consulte a classe
-#'   \code{\link[base]{Dates}}.
+#'   O valor padrão para a variável \code{formato_data} é \code{Ano}, \code{Mes}
+#'   e \code{Dia}. Deve-se ordernar esses três componentes conforme a formatação
+#'   que estiver presente no arquivo do portal Hidroweb.
 #'
 #' @export
-organizar <- function(dados, formato_data = "%d/%m/%Y"){
+organizar <- function(dados, formato_data =  c("Ano", "Mes", "Dia")){
   # Limpando os dados
   if(grepl("Vazao", names(dados)[17])){
     valor <- "Q"
@@ -25,7 +25,7 @@ organizar <- function(dados, formato_data = "%d/%m/%Y"){
   }
   dados <- dados[c(1, 3, 17:47)]
   names(dados) <- c("Estacao", "Data", 1:31)
-  dados <- tidyr::separate(dados, Data, into = c("Ano", "Mes", "Dia"))
+  dados <- tidyr::separate(dados, Data, into = formato_data)
   dados["Dia"] <- NULL
 
   # Organizando
@@ -40,7 +40,7 @@ organizar <- function(dados, formato_data = "%d/%m/%Y"){
   } else{
     dados <- dplyr::filter(dados, !is.na(P))
   }
-  dados$Data <- as.Date(dados$Data, format = formato_data)
+  dados$Data <- as.Date(dados$Data, format = "%d/%m/%Y")
   if(length(unique(dados$Estacao)) > 1){
     dados_bkp <- dados
     dados <- NULL
