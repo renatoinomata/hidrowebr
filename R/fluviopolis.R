21e133024be11417192e8c52a2abbac863eddc0b
renatoinomata <renato.inomata@gmail.com>
2018-09-11 13:48:59 -0300

    Adicionado data da estação

--- R/fluviopolis.R
+++ R/fluviopolis.R
@@ -1,8 +1,8 @@
 #' Dados da estação de Fluviópolis
 #'
-#' @description Dados das vazões (m³/s) do rio Iguaçu na estação de Fluviópolis,
-#'   localizada no município de São Mateus do Sul - PR. Os dados são do período
-#'   de 1963 a 2010.
+#' @description Dados das vazões (m³/s) do rio Iguaçu na estação de Fluviópolis
+#'   (código 65220000), localizada no município de São Mateus do Sul - PR. Os
+#'   dados são do período de 1963 a 2010.
 #'
 #' @name fluviopolis
 NULL
