923f7710b5cd81005bf25268960114545229fd67
renatoinomata <renato.inomata@gmail.com>
2018-10-01 17:38:26 -0300

    adicionado função de qmlp

---
+++ R/qmlp.R
@@ -0,0 +1,18 @@
+#' Vazão média de longa permanência
+#'
+#' @description Calcula a vazão média de longa permanência (longo período) por
+#'   estação.
+#'
+#' @param dados Data frame com a série hidrológica a ser utilizada.
+#' @param col_valores O nome da coluna com os valores a serem utilizados.
+#'
+#' #' @examples # Cálculo da vazão média de longa permanência de fluviópolis:
+#' qmlp(fluviopolis, col_valores = "Q")
+#'
+#' @export
+qmlp <- function(dados, col_valores){
+  Qmlp <- dplyr::group_by(dados, Est)
+  Qmlp <- dplyr::summarise(Qmlp, Qmlp = round(mean(Q), 2))
+  Qmlp <- data.frame(Qmlp)
+  return(Qmlp)
+}
