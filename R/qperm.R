cf73c4e0fbf29180fed85751fd25a5a6cb6c971f
renatoinomata <renato.inomata@gmail.com>
2018-10-15 19:50:12 -0300

    qperm retorna uma data frame com as colunas Qp

--- R/qperm.R
+++ R/qperm.R
@@ -4,9 +4,9 @@
 #'   \code{\link{cperm}}.
 #'
 #' @param cperm Data frame obtida pela função de \code{cperm}.
-#' @param ref Vetor numérico com as frequências para as vazões de permanência.
+#' @param freq Vetor numérico com as frequências para as vazões de permanência.
 #'
-#' @details As vazões de referência são calculadas como a média do valor
+#' @details As vazões de permanência são calculadas como a média do valor
 #'   imediatamente superior e imediatamente inferior à frequência especificada.
 #'
 #' @examples
@@ -17,21 +17,40 @@
 #' qperm(cPerm, 95)
 #'
 #' @export
-qperm <- function(cperm, ref){
+qperm <- function(cperm, freq){
   est <- unique(cperm$Est)
-  qperms <- data.frame()
-  for (i in  1:length(est)){
-    cperm_aux <- dplyr::filter(cperm, Est == est[i])
-    for (j in 1:length(ref)){
-      inferior <- min(dplyr::filter(dplyr::filter(cperm, Est == est[i]),
-                                    Freq <= ref[j])$Q)
-      superior <- max(dplyr::filter(dplyr::filter(cperm, Est == est[i]),
-                                    Freq >= ref[j])$Q)
-      aux <- data.frame(est[i], (superior + inferior)/2, ref[j])
-      qperms <- rbind(qperms, aux)
+  qperms <- data.frame(est)
+  for (i in freq){
+    aux <- c()
+    for (j in est){
+      inferior <- min(dplyr::filter(dplyr::filter(cperm, Est == j),
+                                    Freq <= i)$Q)
+      superior <- max(dplyr::filter(dplyr::filter(cperm, Est == j),
+                                    Freq >= i)$Q)
+      aux <- c(aux, round((superior + inferior)/2, 2))
     }
+    qperms <- data.frame(qperms, aux)
   }
-  names(qperms) <- c("Est", "Q", "Ref")
-  qperms$Q <- round(qperms$Q, 2)
+  names(qperms) <- c("Est", paste0("Q", freq))
   return(qperms)
 }
+# qperm <- function(cperm, freq){
+#   est <- unique(cperm$Est)
+#   qperms <- data.frame()
+#   for (i in  1:length(est)){
+#     cperm_aux <- dplyr::filter(cperm, Est == est[i])
+#     for (j in 1:length(freq)){
+#       inferior <- min(dplyr::filter(dplyr::filter(cperm, Est == est[i]),
+#                                     Freq <= freq[j])$Q)
+#       superior <- max(dplyr::filter(dplyr::filter(cperm, Est == est[i]),
+#                                     Freq >= freq[j])$Q)
+#       aux <- data.frame(est[i], (superior + inferior)/2, freq[j])
+#       qperms <- rbind(qperms, aux)
+#     }
+#   }
+#   names(qperms) <- c("Est", "Q", "Freq")
+#   qperms$Q <- round(qperms$Q, 2)
+#   return(qperms)
+# }
+
+
