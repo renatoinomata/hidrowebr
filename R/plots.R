aa886995ea7899f691dd482e643597b088334e26
renatoinomata <renato.inomata@gmail.com>
2018-11-04 15:46:26 -0200

    )

--- R/plots.R
+++ R/plots.R
@@ -252,7 +252,7 @@ plot_dist <- function(valores, dist, tipo = "Q"){
 
   if(any(dist == "lgamma3")){
     distr[[i]] <- try(fitdistrplus::fitdist(valores, "lgamma3",
-                                      start = list(shape = 2, scale = 2))
+                                      start = list(shape = 2, scale = 2)))
     if(!assertthat::is.error(distr[[i]])){
     i <- i + 1
     } else {
